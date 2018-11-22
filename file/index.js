2446303d43b4ddf3170abf21569b3b2517079bdd
dq_hyf9527 <hyf@bangqu.com>
2018-11-22 17:30:45 +0800

    更新

---
+++ file/index.js
@@ -0,0 +1,30 @@
+
+//----qqget
+this.$ajax({
+  method: 'get',
+  url: '',
+  params: {
+
+  }
+}).then(res=>{
+  console.log(res)
+}).catch(error =>{
+  console.log(error.response)
+});
+//---- qqpost
+this.$ajax({
+  method: 'post',
+  url: '',
+  data: {
+
+  }
+}).then(res=>{
+  console.log(res)
+}).catch(error =>{
+  console.log(error.response)
+});
+
+//----qqtbd
+headers:{'Content-Type':'application/x-www-form-urlencoded'}
+//----qqtjs
+headers:{'Content-Type':'application/json'}
